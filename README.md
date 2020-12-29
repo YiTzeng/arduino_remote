7b235b9d33a41db56e2ee2e5aad4d3234dc43747
Yi-Tzeng <200437546@student.georgianc.on.ca>
2020-12-29 16:47:23 -0500

    lcd feature & reconnection

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # arduino_remote
 It is a small project to simulate action for keyboard with Arduino kit.
+
+Arduino badges:
+    Arduino Leonardo
+    Arduino 321 Maker shield
+    16x2 LCD
+    esp8266
+
+Background:
+    I found that it is hard to control my laptop wirelessly without a bluetooth mouse or a keyboard. I happened to have some Arduino kits which I bought for my course. The Arduino kit did not come with Bluetooth module so that I decided to use the esp866 the conduct wireless communication.
+
+Setup:
+    Laptop, Windows 10 operated, runs a socket server over TCP on a specified port
+    
+    Arduino Leonardo, configured as socket client over TCP on a specified port
+
+Features:
+    Rotate the dial on the board to adjust audio volume
+
+    Press swith 1 to start or pause the video
+
+    Press swith 2 to enter or exit fullscreen
+
+    Display wifi connection with led and lcd
+
+        a. ON on wifi section & red led on as the board is connected to the wifi
+
+        b. OFF on wifi section & blue led on as the board is disconnected to the wifi
+
+    Display socket connection with RGB led and lcd
+
+        a. ON on socket section & RGB led on red channel on as the board is connected to the socket
+
+        b. OFF on socket section & RGB led on blue channel on as the board is disconnected to the socket
+    
+    Connect to wifi & socket automatically
+
+    Reconnect to wifi & socket automatically
--- arduino-controller/arduino-controller.ino
+++ arduino-controller/arduino-controller.ino
@@ -1,42 +1,69 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#define I2C_ADDR 0x27
+#define BACKLIGHT_PIN 3
+
+LiquidCrystal_I2C lcd(I2C_ADDR, 20, 4);
+
 int blueLed = 13;
 int redLed = 12;
 int redRGB = 9;
 int greenRGB = 10;
-int videoController = 2;
+int keySpace = 2;
 int volumeDial = A0;
 int volumeVal;
 int volume;
-int tcpStop = 3;
+int keyF = 3;
+
 //TCP Parameter
-String tcpIp = "\"10.0.0.37\"";
+//replace IP with your IP using ifconfig
+String tcpIp = "\"IP\"";
 String netType = "\"TCP\"";
 String tcpPort = "8080";
 String tcpPara = netType + "," + tcpIp + "," + tcpPort;
 
 //Wifi Parameter
-String ssid = "\"CGNM-88B8\"";
-String pw = "\"251183006782\"";
+//replace ssid & and password with your setting
+String ssid = "\"ssid\"";
+String pw = "\"wifi password\"";
 String wifiPara = ssid + "," + pw;
+
+void checkAllConnections(){
+   if(Serial1.available()){
+      String espMsg = Serial1.readString();
+      if(espMsg.indexOf("WIFI DISCONNECT") != -1){
+        displayWifiStatus(false);
+        connect2Wifi();
+      }
+      if(espMsg.indexOf("CLOSED") != -1){
+        displayScoketStatus(false);
+        connect2socket();
+      }
+   }
+}
 
 void connect2Wifi(){
   String cmd = "AT+CWJAP=" + wifiPara;
   executeCmd(cmd);
-  digitalWrite(redLed, LOW);
-  digitalWrite(blueLed, HIGH);
+  displayWifiStatus(true);
 }
+
+void disconnect2Wifi(){
+  String cmd = "AT+CWQAP";
+  executeCmd(cmd);
+  displayWifiStatus(false);
+} 
 
 void connect2socket(){
   String cmd = "AT+CIPSTART=" + tcpPara;
   executeCmd(cmd);
-  analogWrite(redRGB, 0);
-  analogWrite(greenRGB, 20);
+  displayScoketStatus(true);
 }
 
 void diconnect2socket(){
   String cmd = "AT+CIPCLOSE";
   executeCmd(cmd);
-  analogWrite(redRGB, 20);
-  analogWrite(greenRGB, 0);
+  displayScoketStatus(false);
 }
 
 void sendMsg(String msg){
@@ -60,44 +87,86 @@ boolean cmdCompletion(){
 boolean msgReceived(){
   return Serial1.find("true");
 }
+//Show wifi & socket connection with led & lcd
+void displayWifiStatus(boolean conStatus){
+  if(conStatus){
+    lcd.setCursor(8,0);
+    lcd.print("ON ");
+    digitalWrite(redLed, LOW);
+    digitalWrite(blueLed, HIGH);
+  }
+  else{
+    lcd.setCursor(8,0);
+    lcd.print("OFF");
+    digitalWrite(redLed, HIGH);
+    digitalWrite(blueLed, LOW);
+  }
+}
+
+void displayScoketStatus(boolean conStatus){
+  if(conStatus){
+    lcd.setCursor(8,1);
+    lcd.print("ON ");
+    digitalWrite(redRGB, LOW);
+    digitalWrite(greenRGB, HIGH);
+  }
+  else{
+    lcd.setCursor(8,1);
+    lcd.print("OFF");
+    digitalWrite(redRGB, HIGH);
+    digitalWrite(greenRGB, LOW);
+  }
+}
+
 ////////////////SETUP////////////////
 void setup(){
     pinMode(blueLed,OUTPUT);
     pinMode(redLed,OUTPUT);
     pinMode(redRGB,OUTPUT);
     pinMode(greenRGB,OUTPUT);
-    pinMode(videoController,INPUT);
+    pinMode(keySpace,INPUT);
     pinMode(volumeDial,INPUT);
-    pinMode(tcpStop,INPUT);
-//    Serial.begin(9600);
-    Serial1.setTimeout(1500);
+    pinMode(keyF,INPUT);
     Serial1.begin(115200);
-    while(!Serial1){
-      digitalWrite(redLed, HIGH);
-      analogWrite(redRGB, 20);
-    }
+    Serial1.setTimeout(1500);
+    digitalWrite(redLed, HIGH);
+    digitalWrite(redRGB, HIGH);
+    //lcd begin & setting
+    lcd.init(); // initialize the lcd 
+    lcd.backlight();
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Wifi:");
+    lcd.setCursor(0,1);
+    lcd.print("Socket:");
+    displayWifiStatus(false);
+    displayScoketStatus(false);
+    //wait for connection between esp8266 & arduino board
+    while(!Serial1){}
     connect2Wifi();
     connect2socket();
-    volume = map(analogRead(volumeDial), 0, 1023, 0, 80);
+    //get initial value of dial
+    volume = map(analogRead(volumeDial), 0, 1023, 0, 50);
 }
 
 ////////////////LOOP////////////////
 void loop(){
-  int newVolume = map(analogRead(volumeDial), 0, 1023, 0, 80);
-   delay(100);
+  checkAllConnections();
+  int newVolume = map(analogRead(volumeDial), 0, 1023, 0, 50);
+  delay(100);
+  //send new volume when dial is changed
   if(volume != newVolume){
     volume = newVolume;
     sendMsg(String(volume));
     }
   
-  if(!digitalRead(videoController)){
-      sendMsg("video");
+  if(!digitalRead(keySpace)){
+      sendMsg("space");
       delay(300);
    }
-  if(!digitalRead(tcpStop)){
-      diconnect2socket();
+  if(!digitalRead(keyF)){
+      sendMsg("f");
       delay(300);
    }
-  
   
 }
